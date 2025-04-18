beeee8313821fb56e15e5bad1e03cf496099c016
Ngô Thị Trang Nhung <89560779+Nhung2310@users.noreply.github.com>
2025-04-18 09:58:15 +0700

    FEATURE | #2 Implement Onbording Screen

---
+++ lib/app_assets.dart
@@ -0,0 +1,4 @@
+class AppAssets {
+  static const String icCarot = 'assets/carot.png';
+  static const String icMan = 'assets/man.png';
+}
---
+++ lib/ui/onbording.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:online_groceries_app/app_assets.dart';
+import 'package:online_groceries_app/app_color.dart';
+
+class Onbording extends StatelessWidget {
+  const Onbording({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: Image.asset(AppAssets.icMan, fit: BoxFit.cover),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              child: Center(
+                child: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      width: 60,
+                      height: 60,
+                      AppAssets.icCarot,
+                      fit: BoxFit.contain,
+                    ),
+                    Center(
+                      child: Text(
+                        'Welcome\n to our store',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: AppColor.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Ger your groceries in as fast as one hour',
+                      style: TextStyle(color: AppColor.white, fontSize: 10),
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                          backgroundColor: Colors.green,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
